404cd9d586f07a7268016a30d71f3860eb1e21dc
ryoryoryoooo <P1211065@gmail.com>
2021-09-03 05:41:53 +0900

    最初のコミット

--- SampleSwift1muranaka/ViewController.swift
+++ SampleSwift1muranaka/ViewController.swift
@@ -1,19 +1,66 @@
 //
 //  ViewController.swift
-//  SampleSwift1muranaka
-//
-//  Created by 村中令 on 2021/09/02.
+//  UIKit011
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    // UIPickerView.
+    private var myUIPicker: UIPickerView!
+
+    // 表示する値の配列.
+    private let myValues: NSArray = ["その一","その二","その三","その四"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // UIPickerViewを生成.
+        myUIPicker = UIPickerView()
+
+        // サイズを指定する.
+        myUIPicker.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 180.0)
+
+        // Delegateを設定する.
+        myUIPicker.delegate = self
+
+        // DataSourceを設定する.
+        myUIPicker.dataSource = self
+
+        // Viewに追加する.
+        self.view.addSubview(myUIPicker)
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    /*
+     pickerに表示する行数を返すデータソースメソッド.
+     (実装必須)
+     */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myValues.count
+    }
+
+    /*
+     pickerに表示する値を返すデリゲートメソッド.
+     */
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myValues[row] as? String
+    }
+
+    /*
+     pickerが選択された際に呼ばれるデリゲートメソッド.
+     */
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row: \(row)")
+        print("value: \(myValues[row])")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
